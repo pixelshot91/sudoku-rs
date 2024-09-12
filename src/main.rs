4aebb8bf899e6ef897147846511b5b76f3291b54
Julien Gautier <gautier.julien@gmail.com>
2024-09-12 23:38:43 +0200

    Finish solving, add grid display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use core::str;
+
 use strum::{EnumIter, IntoEnumIterator};
 
 #[derive(Debug, Clone, Copy, EnumIter, PartialEq, Eq)]
@@ -7,17 +9,27 @@ enum Digit {
     Two = 1,
     Three = 2,
     Four = 3,
-    Five = 4,
-    Six = 5,
-    Seven = 6,
-    Height = 7,
-    Nine = 8,
+    // Five = 4,
+    // Six = 5,
+    // Seven = 6,
+    // Height = 7,
+    // Nine = 8,
+}
+impl Digit {
+    fn to_char(&self) -> char {
+        match self {
+            Digit::One => '1',
+            Digit::Two => '2',
+            Digit::Three => '3',
+            Digit::Four => '4',
+        }
+    }
 }
 
 trait Next: Sized {
     fn next(&self) -> Self;
 }
-impl Next for Option<Digit> {
+impl Next for Cell {
     fn next(&self) -> Self {
         match self {
             None => Some(Digit::One),
@@ -26,38 +38,36 @@ impl Next for Option<Digit> {
                 Digit::One => Some(Digit::Two),
                 Digit::Two => Some(Digit::Three),
                 Digit::Three => Some(Digit::Four),
-                Digit::Four => Some(Digit::Five),
-                Digit::Five => Some(Digit::Six),
-                Digit::Six => Some(Digit::Seven),
-                Digit::Seven => Some(Digit::Height),
-                Digit::Height => Some(Digit::Nine),
-                // The digit was Nine, so there is no possible value
-                Digit::Nine => None,
+                Digit::Four => None,
+                // Digit::Four => Some(Digit::Five),
+                // Digit::Five => Some(Digit::Six),
+                // Digit::Six => Some(Digit::Seven),
+                // Digit::Seven => Some(Digit::Height),
+                // Digit::Height => Some(Digit::Nine),
+                // // The digit was Nine, so there is no possible value
+                // Digit::Nine => None,
             },
         }
     }
 }
 
-const BLOCK_SIDE: usize = 3;
+const BLOCK_SIDE: usize = 2;
 const NB_DIGIT: usize = BLOCK_SIDE * BLOCK_SIDE;
 const NB_CELL: usize = NB_DIGIT * NB_DIGIT;
 
-#[derive(Clone, Copy, Debug)]
-struct Cell {
-    value: Option<Digit>,
-}
+type Cell = Option<Digit>;
 
 /// Guarantees that no digit are in direct contradiction
 /// The grid maybe unsolvable though
 #[derive(Clone)]
 struct Grid {
-    data: [Cell; NB_CELL as usize],
+    data: [Cell; NB_CELL],
 }
 
 impl Grid {
     fn empty() -> Grid {
         Grid {
-            data: [Cell { value: None }; NB_CELL],
+            data: [None; NB_CELL],
         }
     }
 
@@ -69,15 +79,13 @@ impl Grid {
     fn can_accept_digit_at_pos(&self, d: Digit, pos: usize) -> bool {
         let line_does_not_contain_digit = || {
             let first_cell_in_line_index = pos / NB_DIGIT * NB_DIGIT;
-            (0..NB_DIGIT)
-                .all(|column| self.data[first_cell_in_line_index + column].value != Some(d))
+            (0..NB_DIGIT).all(|column| self.data[first_cell_in_line_index + column] != Some(d))
         };
 
         let column_does_not_contain_digit = || {
             let first_cell_in_column_index = pos % NB_DIGIT;
-            (0..NB_DIGIT).all(|line| {
-                self.data[first_cell_in_column_index + line * NB_DIGIT].value != Some(d)
-            })
+            (0..NB_DIGIT)
+                .all(|line| self.data[first_cell_in_column_index + line * NB_DIGIT] != Some(d))
         };
 
         let block_does_not_contain_digit = || {
@@ -92,13 +100,125 @@ impl Grid {
                 .all(|line| {
                     (0..BLOCK_SIDE)
                         .map(|x| x + first_cell_in_block_column_index)
-                        .all(|column| self.data[line * NB_DIGIT + column].value != Some(d))
+                        .all(|column| self.data[line * NB_DIGIT + column] != Some(d))
                 })
         };
 
         line_does_not_contain_digit()
             && column_does_not_contain_digit()
             && block_does_not_contain_digit()
+    }
+}
+
+impl std::fmt::Display for Grid {
+    #[allow(unstable_name_collisions)]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use itertools::Itertools;
+
+        fn times(n: usize) -> impl Iterator {
+            std::iter::repeat(()).take(n)
+        }
+
+        const TOP_LEFT_CORNER: char = '┌';
+        const TOP_RIGHT_CORNER: char = '┐';
+        const BOTTOM_RIGHT_CORNER: char = '┘';
+        const BOTTOM_LEFT_CORNER: char = '└';
+
+        const HORIZONTAL_BORDER: char = '─';
+        const VERTICAL_BORDER: char = '│';
+
+        const UP_TEE: &str = "┬";
+        const RIGHT_TEE: char = '┤';
+        const DOWN_TEE: &str = "┴";
+        const LEFT_TEE: char = '├';
+
+        const CROSS: &str = "┼";
+
+        const NB_BLOCK: usize = BLOCK_SIDE;
+
+        let line_length =
+        // All digit will be on the line
+        NB_DIGIT
+        // As many separator as blocks
+        + NB_BLOCK
+        // end of block
+        + 1
+        // new line
+        + 1;
+
+        // TODO: allocate only the right amount, then only use push or push_str, but od not create extra String
+        let mut s = String::with_capacity(line_length * line_length);
+
+        // str::from_utf8(HORIZONTAL_TEE)
+        // vec!["ds", "fds"].iter().as_slice().join(sep);
+
+        // First border line
+        s.push(TOP_LEFT_CORNER);
+        s.push_str(
+            &times(NB_BLOCK)
+                .map(|_| times(BLOCK_SIDE).map(|_| HORIZONTAL_BORDER).join(""))
+                .join(UP_TEE),
+        );
+        s.push(TOP_RIGHT_CORNER);
+        s.push('\n');
+
+        let horizontal_border_line = {
+            let mut s = LEFT_TEE.to_string();
+            s.push_str(
+                &times(BLOCK_SIDE)
+                    .map(|_| times(BLOCK_SIDE).map(|_| HORIZONTAL_BORDER).join(""))
+                    .join(CROSS),
+            );
+            s.push(RIGHT_TEE);
+            s.push('\n');
+            s
+        };
+
+        let body = (0..NB_BLOCK)
+            .map(|block_y_index| {
+                (0..BLOCK_SIDE)
+                    .map(|line_in_block| {
+                        let line = block_y_index * BLOCK_SIDE + line_in_block;
+                        let mut number_line = String::new();
+                        number_line.push(VERTICAL_BORDER);
+                        let number_line_body = (0..NB_BLOCK)
+                            .map(|block_x_index| {
+                                (0..BLOCK_SIDE)
+                                    .map(|column_in_block| {
+                                        let column = block_x_index * BLOCK_SIDE + column_in_block;
+                                        let cell = self.data[line * NB_DIGIT + column];
+                                        match cell {
+                                            None => '.',
+                                            Some(d) => d.to_char(),
+                                        }
+                                    })
+                                    .join("")
+                            })
+                            .join(&VERTICAL_BORDER.to_string());
+                        number_line.push_str(&number_line_body);
+
+                        number_line.push(VERTICAL_BORDER);
+                        number_line.push('\n');
+
+                        number_line
+                    })
+                    .join("")
+            })
+            .join(&horizontal_border_line);
+
+        s.push_str(&body);
+
+        // Bottom border line
+        s.push(BOTTOM_LEFT_CORNER);
+        s.push_str(
+            &times(NB_BLOCK)
+                .map(|_| times(BLOCK_SIDE).map(|_| HORIZONTAL_BORDER).join(""))
+                .join(DOWN_TEE),
+        );
+        s.push(BOTTOM_RIGHT_CORNER);
+        s.push('\n');
+
+        f.write_str(&s)
     }
 }
 
@@ -112,7 +232,7 @@ struct PartialySolvedGrid {
 
 impl PartialySolvedGrid {
     fn try_fill_next_cell(&mut self) -> bool {
-        match self.grid.data[self.fill_until].value {
+        match self.grid.data[self.fill_until] {
             Some(_) => {
                 // a digit is already here
                 self.fill_until += 1;
@@ -132,11 +252,11 @@ impl PartialySolvedGrid {
     }
 
     fn try_increment_cell_at_index(&mut self, cell_index: usize) -> bool {
-        let original_digit = self.grid.data[cell_index].value.take();
+        let original_digit = self.grid.data[cell_index].take();
         let d = original_digit;
         while let Some(d) = d.next() {
             if self.grid.can_accept_digit_at_pos(d, self.fill_until) {
-                self.grid.data[cell_index].value = Some(d);
+                self.grid.data[cell_index] = Some(d);
                 return true;
             }
         }
@@ -160,23 +280,50 @@ impl<'a> GridSolver<'a> {
             },
         }
     }
+
+    // Either fill the next cell, or backtrack until a previous cell can be incremented
+    // If we see the grid digit in a list and interpret that as a number (empty cell meaning 0),
+    // then this number after this function should be strictly greather than before calling the function
+    // Return if a progress has been made
+    // Returning false mean there is no more solution to be found
+    fn make_progress(&mut self) -> bool {
+        match self.psg.try_fill_next_cell() {
+            // The cell has been filled, continue this way
+            true => true,
+            // No cell could have been filled: we are at a dead-end: backtrack
+            false => {
+                fn guessed_cells(
+                    self_psg_fill_until: &usize,
+                    self_initial_grid_data: &[Cell; NB_CELL],
+                ) -> Vec<usize> {
+                    (*self_psg_fill_until..0)
+                        .rev()
+                        // Only keep the cell which were empty in the initial grid
+                        .filter(|cell_index| self_initial_grid_data[*cell_index].is_none())
+                        .collect::<Vec<usize>>()
+                }
+
+                let guessed_cells = guessed_cells(&self.psg.fill_until, &self.initial_grid.data);
+                for guessed_cell in guessed_cells {
+                    if self.psg.try_increment_cell_at_index(guessed_cell) {
+                        // the last guessed cell has been incremented,
+                        // TODO: break out of the little loop, but stay inside the big loop
+                        return true;
+                    }
+                }
+                // Could not increment any of the already filled cells
+                // We already know that the next cannot be filled either
+                // There is no more solution
+                false
+            }
+        }
+    }
 }
 
 impl<'a> Iterator for GridSolver<'a> {
     type Item = SolvedGrid;
 
     fn next(&mut self) -> Option<Self::Item> {
-        fn guessed_cells(
-            self_psg_fill_until: &usize,
-            self_initial_grid_data: &[Cell; NB_CELL],
-        ) -> Vec<usize> {
-            (*self_psg_fill_until..0)
-                .rev()
-                // Only keep the cell which were empty in the initial grid
-                .filter(|cell_index| self_initial_grid_data[*cell_index].value.is_none())
-                .collect::<Vec<usize>>()
-        }
-
         // The only way out of this loop is to either:
         // - return a possible solution
         // - exhaust all possible solution, then return
@@ -185,60 +332,10 @@ impl<'a> Iterator for GridSolver<'a> {
                 return Some(SolvedGrid::from_psg(&self.psg));
             }
 
-            match self.psg.try_fill_next_cell() {
-                // The cell has been filled, continue this way
-                true => {}
-                // No cell could have been filled: we are at a dead-end: backtrack
-                false => {
-                    for guessed_cell in guessed_cells(&self.psg.fill_until, &self.initial_grid.data)
-                    {
-                        if self.psg.try_increment_cell_at_index(guessed_cell) {
-                            // the last guessed cell has been incremented,
-                            // TODO: break out of the little loop, but stay inside the big loop
-                            break;
-                        }
-                    }
-                    // Could not increment any of the already filled cells
-                    // We already know that the next cannot be filled either
-                    // There is no more solution
-                    return None;
-                }
+            if self.make_progress() == false {
+                return None;
             }
         }
-
-        // let data = &mut self.initial_grid.data;
-
-        // for i in 0..data.len() {
-        //     match data[i].value {
-
-        //         Some(_) => {
-        //             // a digit is already here
-        //             continue;
-        //         },
-        //         None => {
-        //             for d in Digit::iter() {
-
-        //             }
-        //             data[i] =
-        //         },
-
-        //     }
-        // }
-
-        /* let empty_cells = data.iter().filter(|cell| cell.value.is_none());
-        for empty_cell in empty_cells {
-            data[0] = Cell {value: Some(Digit::One)}
-        } */
-        None
-        /*  match first_empty_cell {
-            None => {
-
-            },
-
-            Some(_) => {
-                todo!()
-            },
-        } */
     }
 }
 
@@ -248,15 +345,21 @@ impl<'a> Iterator for GridSolver<'a> {
 ///  - All cells are filled
 /// So the grid is solved
 struct SolvedGrid {
-    data: [Digit; NB_CELL],
+    grid: Grid,
+    // data: [Digit; NB_CELL],
 }
 
 impl SolvedGrid {
     fn from_psg(psg: &PartialySolvedGrid) -> SolvedGrid {
         assert_eq!(psg.fill_until, NB_CELL);
+        psg.grid.data.iter().for_each(|c| assert!(c.is_some()));
+
         SolvedGrid {
-            data: psg.grid.data.map(|maybe_digit| maybe_digit.value.expect("Because fill_until == NB_CELL, and data.len() == fill_until, digit should always be Some"))
+            grid: psg.grid.clone(),
         }
+        // SolvedGrid {
+        //     data: psg.grid.data.map(|maybe_digit| maybe_digit.expect("Because fill_until == NB_CELL, and data.len() == fill_until, digit should always be Some"))
+        // }
     }
     // fn from(grid: Grid) -> SolvedGrid {
     //     SolvedGrid {
@@ -265,8 +368,42 @@ impl SolvedGrid {
     // }
 }
 
+impl std::fmt::Display for SolvedGrid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.grid.fmt(f)
+    }
+}
+
 fn main() {
     let mut grid = Grid::empty();
 
-    for solution in grid.try_solve() {}
+    print!("{}", grid);
+
+    return;
+
+    for solution in grid.try_solve() {
+        println!("{}", solution)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::Grid;
+
+    #[test]
+    fn display_empty_grid() {
+        let grid = Grid::empty();
+        let s = grid.to_string();
+        assert_eq!(
+            s,
+            r"┌──┬──┐
+│..│..│
+│..│..│
+├──┼──┤
+│..│..│
+│..│..│
+└──┴──┘
+"
+        );
+    }
 }
